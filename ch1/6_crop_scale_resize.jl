ce1613274084b69a6958cb20fa0f066a38a47a8f
Dmitrijs Cudihins <dc@rax.lv>
2018-03-15 22:49:04 +0200

    Updates to Chapter 1 - scaling and rotation of images

--- ch1/6_crop_scale_resize.jl
+++ ch1/6_crop_scale_resize.jl
@@ -9,3 +9,13 @@ imshow(cropped_image)
 # crop an image (option 2)
 cropped_image_view = view(img, 100:290, 280:540); # (height, width)
 imshow(cropped_image_view)
+
+# resize an image
+source_image = load("sample-images/cats-3061372_640.jpg");
+resized_image = imresize(source_image, (100, 250)); # (height, width)
+imshow(resized_image);
+
+# resize an image (2)
+source_image = load("sample-images/cats-3061372_640.jpg");
+resized_image = imresize(source_image, (200, 200)); # (height, width)
+imshow(resized_image);
---
+++ ch1/7_rotating_images.jl
@@ -0,0 +1,5 @@
+using Images, CoordinateTransformations
+
+img = load("sample-images/cats-3061372_640.jpg");
+tfm = LinearMap(RotMatrix(-pi/3)) # rotate by -60 degrees 
+img = warp(img, tfm)
